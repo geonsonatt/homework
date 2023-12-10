977d568bbec23500156d59a5033425b67ae24fb9
Ivan <gerano.03@mail.ru>
2023-12-10 17:57:47 +0300

    написал самую простую программу

---
+++ Proga/Program.cs
@@ -0,0 +1,10 @@
+﻿// Эта программа просто выводит число 1
+
+class One
+{
+    public static void Main(string[] args)
+    {
+        // Выводим число в консоль.
+        Console.WriteLine(1);
+    }
+}
