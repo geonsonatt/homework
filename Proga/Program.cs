438e949247357c447461fccf1a1f7d99c4a51eac
Ivan <gerano.03@mail.ru>
2023-12-13 21:52:30 +0300

    Domashnee Zadanie

--- Proga/Program.cs
+++ Proga/Program.cs
@@ -1,10 +1,38 @@
-﻿// Эта программа просто выводит число 1
-
-class One
-{
-    public static void Main(string[] args)
+﻿using System; 
+    namespace Math
     {
-        // Выводим число в консоль.
-        Console.WriteLine(1);
+        class Program 
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("введите координату X" );
+                int n1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("введите координату Y" );
+                int n2 = Convert.ToInt32(Console.ReadLine());
+                if (n1 == 0 && n2 == 0)
+                {
+                Console.WriteLine("Точка координат введена неверно");
+                }
+                else  
+                if (n1 > 0 && n2 > 0)
+                {
+                Console.WriteLine("Точка координат находится в 1 четверти");
+                }
+                else if (n1 < 0 && n2 > 0)
+                {
+                Console.WriteLine("Точка координат находится во 2 четверти");
+                }
+                else 
+                    if (n1 < 0 && n2 < 0)
+                {
+                Console.WriteLine("Точка координат находится в 3 четверти");
+                }
+                else  
+                {
+                Console.WriteLine("Точка координат находится в 4 четверти");
+                }
+                
+            }
+            
+        }
     }
-}
