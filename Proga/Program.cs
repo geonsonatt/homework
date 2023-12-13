28d3e09df057fb4883bee1fe3e48c712a9674768
Ivan <gerano.03@mail.ru>
2023-12-13 22:41:00 +0300

    Vtoroe zadanie

--- Proga/Program.cs
+++ Proga/Program.cs
@@ -1,38 +1,36 @@
-﻿using System; 
-    namespace Math
+﻿using System;
+using System.IO.Compression;
+namespace Math
     {
         class Program 
         {
             static void Main(string[] args)
             {
-                Console.WriteLine("введите координату X" );
-                int n1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("введите координату Y" );
-                int n2 = Convert.ToInt32(Console.ReadLine());
-                if (n1 == 0 && n2 == 0)
+            Console.WriteLine("Введите число N" );
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N < 10)
+            {
+            Console.WriteLine(N);
+            }
+                else
+                {  
+                while (N > 0)
                 {
-                Console.WriteLine("Точка координат введена неверно");
-                }
-                else  
-                if (n1 > 0 && n2 > 0)
-                {
-                Console.WriteLine("Точка координат находится в 1 четверти");
-                }
-                else if (n1 < 0 && n2 > 0)
-                {
-                Console.WriteLine("Точка координат находится во 2 четверти");
-                }
-                else 
-                    if (n1 < 0 && n2 < 0)
-                {
-                Console.WriteLine("Точка координат находится в 3 четверти");
-                }
-                else  
-                {
-                Console.WriteLine("Точка координат находится в 4 четверти");
+                int NewN = N % 10; //Получаем остаток числа, для записи   
+                N /= 10;
+                    if (N>0)
+                    {
+                    Console.Write(NewN +",");   
+                    }
+                    else
+                    {
+                    Console.WriteLine(NewN);    
+                    }   
+                }   
                 }
                 
             }
-            
         }
     }
+        
+    
